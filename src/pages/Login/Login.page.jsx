6140164f9121efc592a3bcc3b1ddeec6d1fb4f8a
vivek9142 <vm199841@gmail.com>
2021-12-21 20:00:53 +0530

    added login functionality

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,7 +1,28 @@
+import { Formik,Form } from "formik";
+import InputField from "../../components/InputField/InputField.component";
+import Button from "../../components/Button/Button.component";
+import { userLogin} from "../../redux/actionCreators/userActionCr";
+import { useDispatch } from "react-redux";
+
 const Login = (props)=> {
-  return (
-    <h1>login</h1>
-  );
+    const dispatch = useDispatch();
+    return (
+      <>
+      <h1>login</h1>
+      <Formik initialValues={{
+        email:'',
+        password:''
+      }} onSubmit={val => dispatch(userLogin(val))}>
+        {formik => (
+          <Form>
+            <InputField type='email' name='email'/>
+            <InputField type='password' name='password'/>
+            <Button type='submit'>Submit</Button>
+          </Form>
+        )}
+      </Formik>
+      </>
+    );
 
 }
 
